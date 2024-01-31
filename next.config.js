8b3576c3ad938b0e26d669bc47ffde947fba6b3a
Emmanuel Ma <emmanuel.ma@gmail.com>
2024-01-31 18:06:20 -0500

    config

--- next.config.js
+++ next.config.js
@@ -14,14 +14,14 @@
 // };
 
 // module.exports = nextConfig;
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        //destination: "http://127.0.0.1:5000/:path*",  Proxy to Backend
-        destination: "http://localhost:5000/api/:path*",
-      },
-    ];
-  },
-};
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         //destination: "http://127.0.0.1:5000/:path*",  Proxy to Backend
+//         destination: "http://localhost:5000/api/:path*",
+//       },
+//     ];
+//   },
+// };

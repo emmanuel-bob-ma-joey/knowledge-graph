6fa9da1b2d31b1c19a131edfb3c886be0eeb7749
Emmanuel Ma <emmanuel.ma@gmail.com>
2024-01-31 16:35:41 -0500

    deploy config

--- next.config.js
+++ next.config.js
@@ -14,3 +14,13 @@
 // };
 
 // module.exports = nextConfig;
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:5000/:path*", // Proxy to Backend
+      },
+    ];
+  },
+};

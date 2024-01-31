189282082c81f9743b832c70e3770528a3fd4d16
Emmanuel Ma <emmanuel.ma@gmail.com>
2024-01-31 17:55:06 -0500

    config

--- next.config.js
+++ next.config.js
@@ -19,7 +19,8 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:5000/:path*", // Proxy to Backend
+        //destination: "http://127.0.0.1:5000/:path*",  Proxy to Backend
+        destination: "http://localhost:5000/api/:path*",
       },
     ];
   },
